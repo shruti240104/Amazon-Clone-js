bb0f2f80c5900620e0ef8158d1817000d45c4623
shruti240104 <shrutisingh240504@gmail.com>
2025-07-05 20:11:57 +0530

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -19,7 +19,9 @@ export function renderOrderSummary() {
     const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
     const dateString = deliveryDate.format('dddd, MMMM D');
 
-    cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+    cartSummaryHTML += `<div class="cart-item-container
+    js-cart-item-container
+    js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: ${dateString}
     </div>
@@ -35,14 +37,15 @@ export function renderOrderSummary() {
           <div class="product-price">
             $${formatCurrency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+            <span class="delete-quantity-link link-primary js-delete-link 
+            js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -113,4 +116,4 @@ export function renderOrderSummary() {
     });
   });
 }
-renderOrderSummary();
+//renderOrderSummary();

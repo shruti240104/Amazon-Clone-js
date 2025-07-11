5e612216234c0668c901d87b0157b386c76c7a48
shruti240104 <shrutisingh240504@gmail.com>
2025-07-11 21:56:08 +0530

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderpaymentSummary } from './checkout/paymentSummary.js';
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderpaymentSummary();

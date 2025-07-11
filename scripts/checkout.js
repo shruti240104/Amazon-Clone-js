50717b17e46584054cb60c8e14cd14fe40846ce3
shruti240104 <shrutisingh240504@gmail.com>
2025-07-11 21:09:29 +0530

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import { renderpaymentSummary } from './checkout/paymentSummary.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderpaymentSummary();
